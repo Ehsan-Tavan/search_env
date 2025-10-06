f7a18ef7ff03a0f7c2ed2cfcb7ede0b6fa7e6684
Ehsan-Tavan <tavan.ehsan@gmail.com>
2025-10-06 10:59:21 +0330

    Add save_data_to_db script.

---
+++ src/save_data_to_db.py
@@ -0,0 +1,38 @@
+import argparse
+
+from src.configurations import Config
+from src.vector_db_handler import MilvusHandler
+from src.vector_db_handler import Embedder
+from src.data_loader import JSONLLoader, Chunker
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Search Environment"
+    )
+    parser.add_argument(
+        "-c", "--config",
+        default=None,
+        type=str,
+        help="Config file path (default: None)"
+    )
+
+    args = parser.parse_args()
+
+    # Validate required arguments
+    if args.config is None:
+        raise ValueError("The config argument should be set!")
+
+    config = Config.from_yaml(args.config)
+
+    milvus_obj = MilvusHandler(config=config.milvus)
+    embedder = Embedder(config.model.name)
+    chunker = Chunker(chunk_size=config.data.chunk_size, chunk_overlap=config.data.chunk_overlap)
+
+    texts = JSONLLoader.load_texts(config.data.path, text_field="text")
+    chunks = chunker.chunk_text(texts=texts)
+
+    embeddings = embedder.encode_texts(chunks)
+
+    milvus_obj.insert_data(embeddings, chunks)
+
