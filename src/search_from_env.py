9995214d676649c4f20b7782ab5d346f2129a41f
Ehsan-Tavan <tavan.ehsan@gmail.com>
2025-10-08 11:15:29 +0330

    Add search_from_env script.

---
+++ src/search_from_env.py
@@ -0,0 +1,27 @@
+import argparse
+
+from src.search import search_tool
+from src.configurations import Config
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Search Environment"
+    )
+    parser.add_argument(
+        "-c", "--config",
+        default=None,
+        type=str,
+        help="Config file path (default: None)"
+    )
+
+    args = parser.parse_args()
+
+    # Validate required arguments
+    if args.config is None:
+        raise ValueError("The config argument should be set!")
+
+    config = Config.from_yaml(args.config)
+
+    results = search_tool(query="سعدی در قرن چند میزیست؟", config=config)
+    print(results)
